3068d3abe878b2386aa4ef27553f54651df4c6fa
Ravi Batistel <ravi.batistel@outlook.com>
2023-03-23 09:25:15 -0300

    inicial

---
+++ src/main/java/com/ravioly/controlehotel/ControleHotelApplication.java
@@ -0,0 +1,13 @@
+package com.ravioly.controlehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleHotelApplication.class, args);
+	}
+
+}
